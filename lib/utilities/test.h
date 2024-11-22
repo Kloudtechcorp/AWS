cbffe99e6ae263a5311442973cc68db73da1a451
dreiseu <121779773+dreiseu@users.noreply.github.com>
2024-11-22 14:25:35 +0800

    Reduced max retry of apn from 4 to 3

--- lib/utilities/test.h
+++ lib/utilities/test.h
@@ -47,7 +47,7 @@ TinyGsm modem(SerialAT);
 const int port = 443;
 bool connectedAPN = false;
 int retryCountAPN = 0;
-const int maxRetriesAPN = 4;
+const int maxRetriesAPN = 3;
 bool connectedServer = false;
 int retryCountServer = 0;
 const int maxRetriesServer = 10;
